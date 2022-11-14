69e53ecc7de07446178700d8f636a3d7712f61ad
Hamza Kale <hamza1kale@gmail.com>
2022-11-14 21:42:53 +0300

    Revizasyon
    Oyun içi kasmada sorunlar giderilmesi için kod eklendi ,Level içi paneller tasarıma uygun hale getirildi, Ozellestirme menusu degistirildi, Canvaslar ayarlandı

---
+++ RunControl/Assets/Script/Resulation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resulation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(Screen.currentResolution.width/2,Screen.currentResolution.height/2,true);   
+    }
+
+}
